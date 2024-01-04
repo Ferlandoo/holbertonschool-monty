3de0bdc4705a2e30f8b547d1d7b68921659503e1
Ferlandoo <contact@ferlando.me>
2024-01-04 03:57:40 -0800

    add trim whitespaces

--- monty.h
+++ monty.h
@@ -45,5 +45,6 @@ void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t *stack);
+void trim_whitespace(char *line);
 
 #endif
