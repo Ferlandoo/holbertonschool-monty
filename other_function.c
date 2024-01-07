5f7de3bf46d252c5511bc928c196af501c202d42
Ferlandoo <contact@ferlando.me>
2024-01-07 20:16:05 +0100

    edit add

--- other_function.c
+++ other_function.c
@@ -2,23 +2,21 @@
 
 /**
  * add - function that adds the top two elements of the stack
- * @stack: -holds the value of the stack
- * @line_number: -holds the value for the line number
- * Return: (void)
+ * @stack: holds the value of the stack
+ * @line_number: holds the value for the line number
+ * Return: void
  */
 
 void add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = *stack;
-	int temp_n;
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L<%d>: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	temp_n = temp->n + temp->next->n;
-	temp->next->n = temp_n;
+	temp->next->n += temp->n;
 	*stack = (*stack)->next;
 	free(temp);
 }
