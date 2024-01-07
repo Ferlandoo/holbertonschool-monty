9fdcaacd6df0be8bb96cae114815d75378f3471b
Ferlandoo <contact@ferlando.me>
2024-01-07 15:11:38 -0800

    edit betty

--- monty.h
+++ monty.h
@@ -18,9 +18,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -51,8 +51,8 @@ extern bus_t bus;
 
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 extern stack_t **global_head;
