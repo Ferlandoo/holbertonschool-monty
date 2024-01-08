a43dc499ab3bbcc6580c4c4aba23f0ec5d9dc528
Ferlandoo <contact@ferlando.me>
2024-01-08 10:31:24 +0100

    update code

--- get_func.c
+++ get_func.c
@@ -2,29 +2,27 @@
 
 /**
  * get_op - selects the correct function to perform the operation
- * @token: the operation to perform
- * Return: pointer to the function that corresponds to the operation
+ * @token: The operator passed as an argument to the program
+ *
+ * Return: A pointer to the function that corresponds to the operator,
+ *         or NULL if no such function is found.
  */
 
 void (*get_op(char *token))(stack_t **stack, unsigned int line_number)
 {
-	instruction_t op[] = {
-		{"push", push},
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"nop", nop},
-		{"add", add},
-		{NULL, NULL}
-	};
-	int i = 0;
-
-	while (op[i].opcode)
-	{
-		if (strcmp(token, op[i].opcode) == 0)
-			return (op[i].f);
-		i++;
-	}
+	if (strcmp(token, "push") == 0)
+		return (push);
+	else if (strcmp(token, "pall") == 0)
+		return (pall);
+	else if (strcmp(token, "pint") == 0)
+		return (pint);
+	else if (strcmp(token, "pop") == 0)
+		return (pop);
+	else if (strcmp(token, "swap") == 0)
+		return (swap);
+	else if (strcmp(token, "add") == 0)
+		return (add);
+	else if (strcmp(token, "nop") == 0)
+		return (nop);
 	return (NULL);
 }
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * main - Entry point of the program.
  * @argc: The number of command-line arguments.
@@ -30,16 +31,14 @@ int main(int argc, char *argv[])
 	{
 		line_number++;
 		char *token = strtok(line, " \t\n");
-
 		if (token == NULL || token[0] == '#')
 			continue;
 		void (*op_func)(stack_t **, unsigned int) = get_op(token);
 		if (op_func == NULL)
 		{
 			fprintf(stderr, "L%u: unknown instruction %s\n", line_number, token);
-			free(line);
+			free(line), fclose(script_file);
 			free_stack(&stack);
-			fclose(script_file);
 			exit(EXIT_FAILURE);
 		}
 		op_func(&stack, line_number);
